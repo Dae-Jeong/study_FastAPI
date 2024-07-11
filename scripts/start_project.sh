d1c77dcb169d2cac09ef13bee4597e9059ddb41e
Dae-Jeong <v4chelsea@gmail.com>
2024-07-11 17:32:17 +0900

    :docs: start_project.sh 수정
    프로젝트 명칭을 옵션으로 적으면 활용할 수 있도록 수정
    ./scripts/start_project.sh -p {project_name}

--- scripts/start_project.sh
+++ scripts/start_project.sh
@@ -2,13 +2,13 @@
 
 set -e  # Any subsequent commands which fail will cause the shell script to exit immediately
 
-# Set variables
-IMAGE_NAME="todo-fastapi"
-CONTAINER_NAME="todo-fastapi-container"
+# Set default values
+PROJECT_NAME="todo"
+IMAGE_NAME="${PROJECT_NAME}-fastapi"
+CONTAINER_NAME="${PROJECT_NAME}-fastapi-container"
+DOCKERFILE_PATH="${PROJECT_NAME}/Dockerfile"
 HOST_PORT=8000
 CONTAINER_PORT=8000
-DOCKERFILE_PATH="todo/Dockerfile"
-PROJECT_PATH="todo"
 
 # Function to display error message and exit
 function error_exit {
@@ -17,6 +17,24 @@ function error_exit {
     docker logs $CONTAINER_NAME || echo "No logs available."
     exit 1
 }
+
+
+# Parse command line arguments
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+        -p|--project-name)
+        PROJECT_NAME="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        *)
+        # unknown option
+        echo "Unknown option: $1"
+        exit 1
+        ;;
+    esac
+done
 
 # Ensure the container is stopped and removed on exit
 trap 'docker stop $CONTAINER_NAME || true && docker rm $CONTAINER_NAME || true' EXIT
@@ -30,9 +48,10 @@ if docker ps -a --format '{{.Names}}' | grep -q "^${CONTAINER_NAME}\$"; then
     docker rm $CONTAINER_NAME || error_exit "Failed to remove container."
 fi
 
+
 # Build the Docker image (if needed)
 echo "Building the Docker image..."
-docker build -t $IMAGE_NAME -f $DOCKERFILE_PATH $PROJECT_PATH || error_exit "Failed to build Docker image."
+docker build -t $IMAGE_NAME -f $DOCKERFILE_PATH $PROJECT_NAME || error_exit "Failed to build Docker image."
 
 # Run the Docker container in the foreground
 echo "Running the Docker container..."
